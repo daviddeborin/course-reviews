5f2b594416e320c2f648f0b3850a14e0805a9ad2
Justin Meid <justinmeid@me.com>
2019-04-25 16:28:06 -0500

    update to review list

--- frontend/src/components/course/reviews/review/Review.jsx
+++ frontend/src/components/course/reviews/review/Review.jsx
@@ -1,1 +1,10 @@
+import React, { Component } from "react";
 
+class Review extends Component {
+  state = {};
+  render() {
+    return <div>review content goes here</div>;
+  }
+}
+
+export default Review;
--- frontend/src/components/course/reviews/review/container/ReviewContainer.jsx
+++ frontend/src/components/course/reviews/review/container/ReviewContainer.jsx
@@ -1,9 +1,41 @@
 import React, { Component } from "react";
+import { List, Button, Modal } from "semantic-ui-react";
+import Review from "../Review";
 
 class ReviewContainer extends Component {
-  state = {};
+  state = {
+    Reviews: [
+      {
+        rating: 5
+      },
+      {
+        rating: 2
+      }
+    ]
+  };
   render() {
-    return <div>Review stuff goes here</div>;
+    return (
+      <div>
+        {/* button to add post */}
+        <Modal trigger={<Button>New Review</Button>}>
+          <Modal.Header>New Review</Modal.Header>
+          <Modal.Content />
+        </Modal>
+
+        {/* this is the list of comments  */}
+        <div>
+          <List divided relaxed>
+            {this.state.Reviews.map((p, i) => (
+              <List.Item key={i}>
+                <List.Content>
+                  <Review />
+                </List.Content>
+              </List.Item>
+            ))}
+          </List>
+        </div>
+      </div>
+    );
   }
 }
 
