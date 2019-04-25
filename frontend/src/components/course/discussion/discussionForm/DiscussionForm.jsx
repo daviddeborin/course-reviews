daa03fc885e9576682e18fc5e512d7a4c74a9fdf
Justin Meid <justinmeid@me.com>
2019-04-25 17:52:43 -0500

    updated new discussion post form

--- frontend/src/components/course/discussion/discussionForm/DiscussionForm.jsx
+++ frontend/src/components/course/discussion/discussionForm/DiscussionForm.jsx
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
+import { Form } from "semantic-ui-react";
 
 class DiscussionForm extends Component {
   state = {};
   render() {
-    return <div>Form Goes Here</div>;
+    return (
+      <Form>
+        <Form.TextArea
+          label="Post"
+          placeholder="what do u have to say fucker"
+        />
+        <Form.Button>Submit</Form.Button>
+      </Form>
+    );
   }
 }
 
