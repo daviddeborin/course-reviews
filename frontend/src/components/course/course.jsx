dc14b2d920563b904d4997c377f9b2ad42677088
Eduardo Gonzalez-Martin <eg3@illnois.edu>
2019-04-28 23:03:15 -0500

    Added basic metrics board

--- frontend/src/components/course/course.jsx
+++ frontend/src/components/course/course.jsx
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
 import { Segment, Tab, Menu, Input } from "semantic-ui-react";
+
 import DiscussionContainer from "./discussion/discussionContainer/DiscussionContainer";
 import ReviewContainer from "./reviews/review/container/ReviewContainer";
+import Ratings from 'react-ratings-declarative';
+
+import './course.scss'
 
 // the content of the tab
 const panes = [
@@ -24,7 +28,50 @@ const panes = [
 ];
 
 class Course extends Component {
-  state = {};
+  state = {
+    avgHours: '1-4',
+    avgDifficulty: 4.6,
+    avgRating: 1,
+  };
+
+  getHourColor = () => {
+    const hours = this.state.avgHours;
+    switch (hours) {
+        case '1-4':
+            return 'green';
+        case '5-9':
+            return 'yellowgreen';
+        case '10-14':
+            return 'gold';
+        case '15-19':
+            return 'orange';
+        default:
+            return 'red';
+    }
+  }
+
+  getDifficultyColors = (border, rating = false) => {
+    let diff = this.state.avgDifficulty;
+    if (rating) {
+      diff = this.state.avgRating;
+    }
+    if (diff < 1.5) {
+      return 'green';
+    }
+    if (diff < 2.5) {
+      return 'olive';
+    }
+    if (diff < 3.5) {
+      if (border) {
+        return 'yellow';
+      }
+      return 'gold';
+    }
+    if (diff < 4.5) {
+      return 'orange';
+    }
+    return 'red';
+  }
 
   render() {
     return (
@@ -43,11 +90,46 @@ class Course extends Component {
 
         {/*  Metrics Dashboard   */}
         <Segment.Group horizontal>
-          <Segment>Metric 1</Segment>
-          <Segment>Metric 2</Segment>
-          <Segment>Metric 3</Segment>
-          <Segment>Metric 4</Segment>
-          <Segment>Metric 5..</Segment>
+          <Segment className='metric' textAlign='center'>
+            Average Hours Per Week
+            <Segment className='centerMetric' color={this.getHourColor()}>
+              {this.state.avgHours}
+            </Segment>
+          </Segment>
+
+          <Segment className='metric' textAlign='center'>
+            Average Rating
+            <Segment className='centerMetric' color='yellow'>
+              <Ratings
+                rating={this.state.avgRating}
+                widgetRatedColors={'gold'}
+                widgetDimensions='1.5em'
+              >
+                <Ratings.Widget/>
+                <Ratings.Widget/>
+                <Ratings.Widget/>
+                <Ratings.Widget/>
+                <Ratings.Widget />
+              </Ratings>
+            </Segment>
+          </Segment>
+
+          <Segment className='metric' textAlign='center'>
+            Average Difficulty
+            <Segment className='centerMetric' color={this.getDifficultyColors(true)}>
+              <Ratings
+                rating={this.state.avgDifficulty}
+                widgetRatedColors={this.getDifficultyColors(false)}
+                widgetDimensions='1.5em'
+              >
+                <Ratings.Widget/>
+                <Ratings.Widget/>
+                <Ratings.Widget/>
+                <Ratings.Widget/>
+                <Ratings.Widget />
+              </Ratings>
+            </Segment>
+          </Segment>
         </Segment.Group>
 
         {/* Discussion/Review Tabs  */}
