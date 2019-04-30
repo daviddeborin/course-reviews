9596dcc815bbe7f1cb824d89e0dab0a9866a0672
Justin Meid <justinmeid@me.com>
2019-04-30 13:39:45 -0500

    updated schemas and db

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,114 @@
-var Sequelize = require('sequelize');
+var Sequelize = require("sequelize");
 
 const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: './database.sqlite'
-})
+  dialect: "sqlite",
+  storage: "./course_review.db"
+});
 
-sequelize.authenticate();
+sequelize.authenticate().then(
+  function(err) {
+    console.log("Connection to db has been established successfully.");
+  },
+  function(err) {
+    console.log("Unable to connect to the database:", err);
+  }
+);
 
-// Models
-var Subject = sequelize.define('subject', {
-    name : {
-        type: Sequelize.STRING,
-        allowNull : false
-    },
-    courses : Sequelize.ARRAY,
-})
+var Course = sequelize.define("Course", {
+  number: Sequelize.TEXT,
+  subject: Sequelize.TEXT,
+  title: Sequelize.TEXT,
+  rating: Sequelize.INTEGER,
+  difficulty: Sequelize.INTEGER,
+  number_of_reviews: Sequelize.INTEGER,
+  hours_1_4: Sequelize.INTEGER,
+  hours_5_9: Sequelize.INTEGER,
+  hours_10_14: Sequelize.INTEGER,
+  hours_15_19: Sequelize.INTEGER,
+  hours_20: Sequelize.INTEGER
+});
 
-var Course = sequelize.define('course', {
-    number : Sequelize.NUMBER,
-    description : Sequelize.STRING
-})
+var User = sequelize.define("User", {
+  username: Sequelize.STRING,
+  password: Sequelize.STRING
+});
 
-sequelize.sync({force: true});
+var Comment = sequelize.define("Comment", {
+  course: {
+    type: Sequelize.INTEGER,
+    references: {
+      model: Course,
+      key: "id"
+    }
+  },
+  user: {
+    type: Sequelize.INTEGER,
+    references: {
+      model: User,
+      key: "id"
+    }
+  },
+  comment: Sequelize.TEXT
+});
 
-module.exports = {Subject : Subject, Course : Course};
+var Review = sequelize.define("Review", {
+  review: Sequelize.TEXT,
+  course: {
+    type: Sequelize.INTEGER,
+    references: {
+      model: Course,
+      key: "id"
+    }
+  },
+  professor: Sequelize.TEXT,
+  date_posted: Sequelize.DATE,
+  user: {
+    type: Sequelize.INTEGER,
+    references: {
+      model: User,
+      key: "id"
+    }
+  },
+  hours: Sequelize.TEXT,
+  difficulty: Sequelize.INTEGER,
+  rating: Sequelize.INTEGER
+});
+
+sequelize.sync({ force: true }).then(
+  function(err) {
+    console.log("sql has been synced");
+  },
+  function(err) {
+    console.log("An error occurred while creating the table:", err);
+  }
+);
+
+// var Course = sequelize.define('User', {
+//     username: Sequelize.STRING,
+//     password: Sequelize.STRING
+// });
+
+// const sequelize = new Sequelize({
+//     dialect: 'sqlite',
+//     storage: './database.sqlite'
+// })
+
+// sequelize.authenticate();
+
+// // Models
+// var Subject = sequelize.define('subject', {
+//     name : {
+//         type: Sequelize.STRING,
+//         allowNull : false
+//     },
+//     courses : Sequelize.ARRAY,
+// })
+
+// var Course = sequelize.define('course', {
+//     number : Sequelize.NUMBER,
+//     description : Sequelize.STRING
+// })
+
+// sequelize.sync({force: true});
+
+// module.exports = {Subject : Subject, Course : Course};
