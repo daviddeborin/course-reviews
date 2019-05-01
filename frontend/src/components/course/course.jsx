9141bab04a81376e3347bd9c2de81df335775a36
Justin Meid <justinmeid@me.com>
2019-04-30 20:02:33 -0500

    fetch course info and set state

--- frontend/src/components/course/course.jsx
+++ frontend/src/components/course/course.jsx
@@ -3,52 +3,42 @@ import { Segment, Tab, Menu, Input } from "semantic-ui-react";
 
 import DiscussionContainer from "./discussion/discussionContainer/DiscussionContainer";
 import ReviewContainer from "./reviews/review/container/ReviewContainer";
-import Ratings from 'react-ratings-declarative';
-
-import './course.scss'
-
-// the content of the tab
-const panes = [
-  {
-    menuItem: "Reviews",
-    render: () => (
-      <Tab.Pane>
-        <ReviewContainer />
-      </Tab.Pane>
-    )
-  },
-  {
-    menuItem: "Discussion",
-    render: () => (
-      <Tab.Pane>
-        <DiscussionContainer />
-      </Tab.Pane>
-    )
-  }
-];
+import Ratings from "react-ratings-declarative";
+import axios from "axios";
+import "./course.scss";
 
 class Course extends Component {
   state = {
-    avgHours: '1-4',
+    avgHours: "1-4",
     avgDifficulty: 4.6,
     avgRating: 1,
+    courseInfo: {}
   };
+
+  componentWillMount() {
+    var url = "http://localhost:9000/course/" + this.props.match.params.id;
+
+    axios.get(url).then(res => {
+      console.log(res.data);
+      this.setState({ courseInfo: res.data });
+    });
+  }
 
   getHourColor = () => {
     const hours = this.state.avgHours;
     switch (hours) {
-        case '1-4':
-            return 'green';
-        case '5-9':
-            return 'yellowgreen';
-        case '10-14':
-            return 'gold';
-        case '15-19':
-            return 'orange';
-        default:
-            return 'red';
+      case "1-4":
+        return "green";
+      case "5-9":
+        return "yellowgreen";
+      case "10-14":
+        return "gold";
+      case "15-19":
+        return "orange";
+      default:
+        return "red";
     }
-  }
+  };
 
   getDifficultyColors = (border, rating = false) => {
     let diff = this.state.avgDifficulty;
@@ -56,31 +46,50 @@ class Course extends Component {
       diff = this.state.avgRating;
     }
     if (diff < 1.5) {
-      return 'green';
+      return "green";
     }
     if (diff < 2.5) {
-      return 'olive';
+      return "olive";
     }
     if (diff < 3.5) {
       if (border) {
-        return 'yellow';
+        return "yellow";
       }
-      return 'gold';
+      return "gold";
     }
     if (diff < 4.5) {
-      return 'orange';
+      return "orange";
     }
-    return 'red';
-  }
+    return "red";
+  };
 
   render() {
+    const panes = [
+      {
+        menuItem: "Reviews",
+        render: () => (
+          <Tab.Pane>
+            <ReviewContainer courseID={this.props.match.params.id} />
+          </Tab.Pane>
+        )
+      },
+      {
+        menuItem: "Discussion",
+        render: () => (
+          <Tab.Pane>
+            <DiscussionContainer courseID={this.props.match.params.id} />
+          </Tab.Pane>
+        )
+      }
+    ];
+
     return (
       <div>
         {/*  Course Title  */}
         {/* <h1>CS 498 Applied Machine Learning</h1> */}
         <Menu borderless>
           <Menu.Item position="left">
-            <h1>CS 498 Applied Machine Learning</h1>
+            <h1>{this.state.courseInfo.title}</h1>
           </Menu.Item>
 
           <Menu.Item position="right">
@@ -90,42 +99,45 @@ class Course extends Component {
 
         {/*  Metrics Dashboard   */}
         <Segment.Group horizontal>
-          <Segment className='metric' textAlign='center'>
+          <Segment className="metric" textAlign="center">
             Average Hours Per Week
-            <Segment className='centerMetric' color={this.getHourColor()}>
+            <Segment className="centerMetric" color={this.getHourColor()}>
               {this.state.avgHours}
             </Segment>
           </Segment>
 
-          <Segment className='metric' textAlign='center'>
+          <Segment className="metric" textAlign="center">
             Average Rating
-            <Segment className='centerMetric' color='yellow'>
+            <Segment className="centerMetric" color="yellow">
               <Ratings
                 rating={this.state.avgRating}
-                widgetRatedColors={'gold'}
-                widgetDimensions='1.5em'
+                widgetRatedColors={"gold"}
+                widgetDimensions="1.5em"
               >
-                <Ratings.Widget/>
-                <Ratings.Widget/>
-                <Ratings.Widget/>
-                <Ratings.Widget/>
+                <Ratings.Widget />
+                <Ratings.Widget />
+                <Ratings.Widget />
+                <Ratings.Widget />
                 <Ratings.Widget />
               </Ratings>
             </Segment>
           </Segment>
 
-          <Segment className='metric' textAlign='center'>
+          <Segment className="metric" textAlign="center">
             Average Difficulty
-            <Segment className='centerMetric' color={this.getDifficultyColors(true)}>
+            <Segment
+              className="centerMetric"
+              color={this.getDifficultyColors(true)}
+            >
               <Ratings
                 rating={this.state.avgDifficulty}
                 widgetRatedColors={this.getDifficultyColors(false)}
-                widgetDimensions='1.5em'
+                widgetDimensions="1.5em"
               >
-                <Ratings.Widget/>
-                <Ratings.Widget/>
-                <Ratings.Widget/>
-                <Ratings.Widget/>
+                <Ratings.Widget />
+                <Ratings.Widget />
+                <Ratings.Widget />
+                <Ratings.Widget />
                 <Ratings.Widget />
               </Ratings>
             </Segment>
