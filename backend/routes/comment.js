5ecba230f140847b2cca87bee4c0acf23c039be3
Justin Meid <justinmeid@me.com>
2019-04-30 19:06:44 -0500

    comment routes

--- backend/routes/comment.js
+++ backend/routes/comment.js
@@ -3,8 +3,21 @@ module.exports = db => {
   var router = express.Router();
 
   // post new comment
+  // something like this: http://localhost:9000/comment/?user=1&course=1&comment=yo%20yo
   router.post("/", function(req, res, next) {
-    res.json({ message: "comment", name: "hi" });
+    db.Comment.create(req.query).then(err => {
+      res.json(err);
+    });
+  });
+
+  // get all comments for a course id
+  router.get("/:id", function(req, res, next) {
+    const id = req.params.id;
+    db.Comment.findAll({
+      where: { course: id }
+    }).then(todo => {
+      res.json(todo);
+    });
   });
 
   return router;
