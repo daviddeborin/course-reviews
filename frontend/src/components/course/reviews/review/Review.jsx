30834de763707d53bc04bf48687667bbde646cc2
Jaskirat Vig <jvig2@illinois.edu>
2019-05-01 01:35:28 -0500

    finished review

--- frontend/src/components/course/reviews/review/Review.jsx
+++ frontend/src/components/course/reviews/review/Review.jsx
@@ -1,17 +1,69 @@
 import React, { Component } from "react";
-import { Rating, Button } from "semantic-ui-react";
+
+import { Segment} from "semantic-ui-react";
 
 class Review extends Component {
-  state = {};
+  state = {
+    termTaken: 'SP17',
+    professor: 'Jaskirat Vig',
+    overallRating: '7.8',
+    review: "This was a v nice class. Gr8 job.",
+  };
+
+  /*getRatingColor = (border, rating = false) => {
+    let diff = this.state.overallRating;
+    if (diff > 8) {
+      return 'green';
+    }
+    if (diff > 6) {
+      return 'yellowgreen';
+    }
+    if (diff > 4) {
+      return 'gold';
+    }
+    if (diff > 2) {
+      return 'orange';
+    }
+    return 'red';
+  }*/
+
   render() {
     return (
       <div>
-        <h2>Term Taken:</h2>
-        <h3>lorem ipsum</h3>
-        <h2>Professor:</h2>
-        <h3>lorem ipsum</h3>
-        <h2>Overall Rating:</h2>
-        <h3>lorem ipsum</h3>
+
+        {/*  Metrics Dashboard   */}
+        <Segment.Group horizontal>
+          <Segment className='metric' textAlign='center'>
+            Term Taken
+            <Segment className='centerMetric' >
+              {this.state.termTaken}
+            </Segment>
+          </Segment>
+
+          <Segment className='metric' textAlign='center'>
+            Professor
+            <Segment className='centerMetric'>
+                {this.state.professor}
+            </Segment>
+          </Segment>
+
+          <Segment className='metric' textAlign='center'>
+            Overall Rating
+            <Segment className='centerMetric' /*color={this.getRatingColor(true)}*/>
+              {this.state.overallRating}
+            </Segment>
+          </Segment>
+        </Segment.Group>
+
+        <Segment.Group horizontal>
+        <Segment className='metric' textAlign='center'>
+            Review
+            <Segment className='centerMetric' >
+              {this.state.review}
+            </Segment>
+          </Segment>
+        </Segment.Group>
+
       </div>
     );
   }
