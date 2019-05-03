dadd12f145f5d4c304e7523e517252929760060e
daviddeborin <daviddeborin@gmail.com>
2019-05-03 13:21:52 -0500

    Updates

---
+++ precache-manifest.c70822a506b7a2d8f7b17147b9685cb5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b290f77ad41557fb7b4a90afdffdb096",
+    "url": "./static/media/Mosk Normal 400.b290f77a.ttf"
+  },
+  {
+    "revision": "994d425b353dbc804c8d82987ec774e9",
+    "url": "./static/media/Mosk Medium 500.994d425b.ttf"
+  },
+  {
+    "revision": "196802937095db7de8a477e20109c6d5",
+    "url": "./static/media/Graviola-Regular.19680293.otf"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "dde8c663571d801408fc",
+    "url": "./static/js/main.ac7aa886.chunk.js"
+  },
+  {
+    "revision": "29c599e2d71cb50db43b",
+    "url": "./static/js/2.baacb06d.chunk.js"
+  },
+  {
+    "revision": "dde8c663571d801408fc",
+    "url": "./static/css/main.3a88d4ef.chunk.css"
+  },
+  {
+    "revision": "e69902e436c48fb2d7bd67fb5c7958e9",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/daviddeborin.github.io/course-reviews/precache-manifest.63e38adc689b48e46f59b121ee640691.js"
+  "./precache-manifest.c70822a506b7a2d8f7b17147b9685cb5.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/daviddeborin.github.io/course-reviews/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
