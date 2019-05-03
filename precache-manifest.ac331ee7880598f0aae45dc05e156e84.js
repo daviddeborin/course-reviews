e07793415f37fb4d80f4514f088e804f9b139893
Justin Meid <justinmeid@me.com>
2019-05-03 12:51:33 -0500

    Updates

---
+++ precache-manifest.ac331ee7880598f0aae45dc05e156e84.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b290f77ad41557fb7b4a90afdffdb096",
+    "url": "/daviddeborin.github.io/course-reviews/static/media/Mosk Normal 400.b290f77a.ttf"
+  },
+  {
+    "revision": "994d425b353dbc804c8d82987ec774e9",
+    "url": "/daviddeborin.github.io/course-reviews/static/media/Mosk Medium 500.994d425b.ttf"
+  },
+  {
+    "revision": "196802937095db7de8a477e20109c6d5",
+    "url": "/daviddeborin.github.io/course-reviews/static/media/Graviola-Regular.19680293.otf"
+  },
+  {
+    "revision": "0d83e6356d01f9583067",
+    "url": "/daviddeborin.github.io/course-reviews/static/js/runtime~main.7a76d8b9.js"
+  },
+  {
+    "revision": "1ce48e73a9b48888dbb2",
+    "url": "/daviddeborin.github.io/course-reviews/static/js/main.b23071d8.chunk.js"
+  },
+  {
+    "revision": "29c599e2d71cb50db43b",
+    "url": "/daviddeborin.github.io/course-reviews/static/js/2.baacb06d.chunk.js"
+  },
+  {
+    "revision": "1ce48e73a9b48888dbb2",
+    "url": "/daviddeborin.github.io/course-reviews/static/css/main.6c823e2c.chunk.css"
+  },
+  {
+    "revision": "1723be9a2e7ea426462588016084d992",
+    "url": "/daviddeborin.github.io/course-reviews/index.html"
+  }
+];
